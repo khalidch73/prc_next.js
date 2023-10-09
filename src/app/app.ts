97188e3f7911313dd55363ed667ca49a1072b852
khalidch73 <khalidnawazch73@gmail.com>
2023-10-09 19:43:53 +0500

    UP 09

---
+++ src/app/app.ts
@@ -0,0 +1,7 @@
+// what is tailwind 
+//https://tailwindcss.com/
+// what is ESLINT  =====> set of instruction how to use your code 
+//what is src directory ======> all folder in a single 
+// .eslintrc.json ====> decide all roles here for code
+// tsconfig.json ====> moderesulation===> bundler to node if any error show in your project 
+// component k folder k andar wo components banty hai jo reuseable hai.
