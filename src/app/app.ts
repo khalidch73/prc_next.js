3db0a8b8e5279f5f358c9e66e95c28bd75db289d
khalidch73 <khalidnawazch73@gmail.com>
2023-10-10 19:12:45 +0500

    upe

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,3 +5,10 @@
 // .eslintrc.json ====> decide all roles here for code
 // tsconfig.json ====> moderesulation===> bundler to node if any error show in your project 
 // component k folder k andar wo components banty hai jo reuseable hai.
+
+
+// basic structure of a website home page 
+//1. logo 
+//2. navigation bar 
+//3. address 
+//4. form 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        HOME PAGE
-      </div>
+    <main className="flex min-h-screen flex-col items-center p-5">
+      <h1>
+        Home page
+      </h1>
+      <button>click here</button>
     </main>
   )
+}
+const user =()=>{
+  return
+  <div>
+    <h1>user name is</h1>
+  </div>
 }
