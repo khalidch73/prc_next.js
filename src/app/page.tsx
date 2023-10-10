1c6020996f5ef8d48f405fabfc2a4e707b40bf81
khalidch73 <khalidnawazch73@gmail.com>
2023-10-10 19:54:38 +0500

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-5">
-      <h1>
+      <h1 className='font-bold'>
         Home page
       </h1>
-      <button>click here</button>
     </main>
   )
-}
-const user =()=>{
-  return
-  <div>
-    <h1>user name is</h1>
-  </div>
 }
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+export default function post() {
+    return (
+      <main className="flex min-h-screen flex-col items-center p-5">
+        <h1 className='font-bold'>
+          post
+        </h1>
+      </main>
+    )
+  }
